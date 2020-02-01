7ecf2374366261eef7b63ef26a70f6594cce6ecb
tganz <tganz5890@googlemail.com>
2020-02-01 08:01:30 +0100

    adds like/commen count to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_052109) do
+ActiveRecord::Schema.define(version: 2020_02_01_070011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 2020_01_28_052109) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.integer "total_likes_count"
+    t.integer "total_comments_count"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
